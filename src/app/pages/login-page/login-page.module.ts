f55f762b368ab1ce64ed8ef925400de5d42ecb10
Li <xingyi.a.li@accenture.com>
2018-10-08 14:19:56 +0800

    Add Flex Layout for Login Page

--- src/app/pages/login-page/login-page.module.ts
+++ src/app/pages/login-page/login-page.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from '../../_components/login/login.component';
 import { LoginPageComponent } from './login-page.component';
 import { loginRouter } from './login-page.router';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
 import { NgZorroAntdModule } from 'ng-zorro-antd';
 
 @NgModule({
   imports: [
     CommonModule,
     loginRouter,
+    FormsModule,
     ReactiveFormsModule,
     NgZorroAntdModule,
   ],
