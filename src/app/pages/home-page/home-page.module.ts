beebb5c62040d35d1c2731754f373d9f56ca6828
Li <xingyi.a.li@accenture.com>
2018-10-08 16:53:03 +0800

    Add header, sider, content, footer component for home-page.

--- src/app/pages/home-page/home-page.module.ts
+++ src/app/pages/home-page/home-page.module.ts
@@ -1,13 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ContentComponent} from '../../_components/content/content.component';
+import { FooterComponent } from '../../_components/footer/footer.component';
+import { HeaderComponent} from '../../_components/header/header.component';
+import { SiderComponent } from '../../_components/sider/sider.component';
 import { HomePageComponent } from './home-page.component';
 import { homeRouter } from './home-page.router';
+import { NgZorroAntdModule } from 'ng-zorro-antd';
 
 @NgModule({
   imports: [
     CommonModule,
     homeRouter,
+    NgZorroAntdModule,
   ],
-  declarations: [HomePageComponent]
+  declarations: [
+    ContentComponent,
+    FooterComponent,
+    HeaderComponent,
+    SiderComponent,
+    HomePageComponent,
+  ]
 })
 export class HomePageModule { }
