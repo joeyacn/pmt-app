d428856c3b5a64b874e0c126679601dc0f6b21e8
Li <xingyi.a.li@accenture.com>
2018-10-08 17:15:48 +0800

    Add menu component

--- src/app/pages/home-page/home-page.module.ts
+++ src/app/pages/home-page/home-page.module.ts
@@ -4,6 +4,7 @@ import { ContentComponent} from '../../_components/content/content.component';
 import { FooterComponent } from '../../_components/footer/footer.component';
 import { HeaderComponent} from '../../_components/header/header.component';
 import { SiderComponent } from '../../_components/sider/sider.component';
+import { MenuComponent } from '../../_components/menu/menu.component';
 import { HomePageComponent } from './home-page.component';
 import { homeRouter } from './home-page.router';
 import { NgZorroAntdModule } from 'ng-zorro-antd';
@@ -19,6 +20,7 @@ import { NgZorroAntdModule } from 'ng-zorro-antd';
     FooterComponent,
     HeaderComponent,
     SiderComponent,
+    MenuComponent,
     HomePageComponent,
   ]
 })
