76b0fb9475ef66ca4643d9ed2a2b4ca2595bbe41
Li <xingyi.a.li@accenture.com>
2018-10-09 11:47:49 +0800

    Update home-page layout and menu collapsible

--- src/app/_components/sider/sider.component.ts
+++ src/app/_components/sider/sider.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sider',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sider.component.css']
 })
 export class SiderComponent implements OnInit {
+
+  @Input() isCollapsed;
 
   constructor() { }
 
